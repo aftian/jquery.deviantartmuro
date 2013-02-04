c5de98494daa9ccffa593eedfafdaeaf7215273c
Sam Graham <git@illusori.co.uk>
2013-02-04 16:05:41 +0000

    Initial commit.

---
+++ examples/example.js
@@ -0,0 +1,26 @@
+(function (window, $, undefined) {
+"use strict";
+
+$('#damuro-goes-here').damuro({
+    background: '../images/crane_squared_by_mudimba_and_draweverywhere.png',
+    splashText: 'Click dat thang to get phat wit dA muro',
+    splashCss: {
+        color: '#33a'
+        },
+    autoload: false
+    })
+    .one('click', function () { $(this).damuro().open(); })
+    .damuro()
+    .done(function (data) {
+            if (data.image && !/\'/.test(data.image)) {
+                $('body').css('backgroundImage', "url('" + data.image + "')");
+            }
+            $(this).hide().damuro().remove();
+            $(this).hide();
+        })
+    .fail(function (data) {
+            $(this).hide().damuro().remove();
+            $('body').append('<p>All aboard the fail whale: ' + data.error + '.</p>');
+        });
+
+})(window, jQuery);
